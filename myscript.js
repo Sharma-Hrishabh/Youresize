bc0734cb72a3837168154f763944089e53abfb4e
ujjwal0501 <ujjwalkumar0501@gmail.com>
2018-08-07 19:35:25 +0530

    changed height with respect to window size

--- myscript.js
+++ myscript.js
@@ -1,4 +1,6 @@
 const player=document.querySelector('.html5-video-player');
-player.style.height="1200px";
-player.style.width="600pxpx";
+var modWidth = "" + window.innerWidth*0.98 + "px";
+var modHeight = "" + window.innerHeight*0.92 + "px";
+player.style.width= modWidth;
+player.style.height= modHeight;
 console.log('done');
